52e066d557587d819ab0db64beaf98af29f03129
clincejoshy <clincepro@gmail.com>
2016-11-21 21:55:52 +0530

    first testing
    Padachone..ingalu kaatholeee

---
+++ notification.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO notifications (notification_num,notification)
+VALUES ('1', 'This is a sample notification')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ token.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "mysql.hostinger.co.uk";
+$username = "u822806530_clinc";
+$password = "qwerty";
+$dbname = "u822806530_db1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO token (token, count)
+VALUES ('ef7e3c7a84649ef2bb34a5f38548db629660b2071461d5bcad8ac2f2d801b11d', '5')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
